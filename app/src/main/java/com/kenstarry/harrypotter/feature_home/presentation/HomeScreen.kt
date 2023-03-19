8cdb08e9039d1017a00d0f081f41537f5cecd847
KenStarry <starrycodes@gmail.com>
2023-03-19 21:33:51 +0300

    Added Home App Bar

--- app/src/main/java/com/kenstarry/harrypotter/feature_home/presentation/HomeScreen.kt
+++ app/src/main/java/com/kenstarry/harrypotter/feature_home/presentation/HomeScreen.kt
@@ -1,15 +1,26 @@
 package com.kenstarry.harrypotter.feature_home.presentation
 
 import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.canopas.lib.showcase.IntroShowCaseScaffold
 import com.kenstarry.harrypotter.core.domain.model.CoreEvents
 import com.kenstarry.harrypotter.core.presentation.viewmodel.CoreViewModel
 import com.kenstarry.harrypotter.feature_home.domain.model.ResponseObserver
+import com.kenstarry.harrypotter.feature_home.presentation.components.HomeTopBar
 
 @Composable
 fun HomeScreen(
@@ -18,9 +29,10 @@ fun HomeScreen(
 
     val coreVM: CoreViewModel = hiltViewModel()
     val lifeCyclerOwner = LocalLifecycleOwner.current
+
     val responseObserver = remember {
 
-        ResponseObserver(){ response ->
+        ResponseObserver() { response ->
             response.body()?.get(0)?.let {
                 Log.d("response", it.name)
                 Log.d("response", it.house)
@@ -40,4 +52,63 @@ fun HomeScreen(
             coreVM.harryPotterCharacters.removeObserver(responseObserver)
         }
     }
+
+    var showAppIntro by remember {
+        mutableStateOf(false)
+    }
+
+    IntroShowCaseScaffold(
+        showIntroShowCase = showAppIntro,
+        onShowCaseCompleted = { showAppIntro = false }
+    ) {
+
+        Scaffold(
+            topBar = {
+                HomeTopBar(
+                    onSearch = {},
+                    onMore = {}
+                )
+            }
+        ) { contentPadding ->
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding)
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.onPrimary)
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+
+                }
+
+            }
+
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/kenstarry/harrypotter/feature_home/presentation/components/HomeTopBar.kt
@@ -0,0 +1,47 @@
+package com.kenstarry.harrypotter.feature_home.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(
+    onSearch: () -> Unit,
+    onMore: () -> Unit
+) {
+
+    LargeTopAppBar(
+        title = {
+            Text(
+                text = "Hogwarts",
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontWeight = FontWeight.ExtraBold
+            )
+        },
+
+        colors = TopAppBarDefaults.largeTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+            titleContentColor = MaterialTheme.colorScheme.secondary
+        ),
+
+        actions = {
+            IconButton(onClick = onSearch) {
+                Icon(
+                    imageVector = Icons.Outlined.Search,
+                    contentDescription = "Search icon"
+                )
+            }
+
+            IconButton(onClick = onMore) {
+                Icon(
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = "More icon"
+                )
+            }
+        }
+    )
+}
