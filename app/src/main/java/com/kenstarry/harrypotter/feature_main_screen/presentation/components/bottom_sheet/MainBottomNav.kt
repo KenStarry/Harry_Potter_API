1de38703189849f777b26d43862b1f06de40c5ba
KenStarry <starrycodes@gmail.com>
2023-03-19 21:24:01 +0300

    Customized bottom navbar

--- app/src/main/java/com/kenstarry/harrypotter/feature_main_screen/presentation/components/bottom_sheet/MainBottomNav.kt
+++ app/src/main/java/com/kenstarry/harrypotter/feature_main_screen/presentation/components/bottom_sheet/MainBottomNav.kt
@@ -1,13 +1,17 @@
 package com.kenstarry.harrypotter.feature_main_screen.presentation.components.bottom_sheet
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -39,17 +43,30 @@ fun MainBottomNav(
 
     //  show / hide bottom bar depending on the current screen
     AnimatedVisibility(visible = isBottomBarVisible.value) {
-        NavigationBar(
-            containerColor = MaterialTheme.colorScheme.onPrimary,
-            windowInsets = WindowInsets(16.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(bottom = 16.dp),
+            contentAlignment = Alignment.Center
         ) {
 
-            screens.forEach {
-                MainBottomNavItem(
-                    navHostController = navHostController,
-                    currentDestination = currentDestination,
-                    screen = it
-                )
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.onSecondary,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(32.dp))
+                    .fillMaxWidth(0.8f)
+                    .wrapContentHeight()
+            ) {
+
+                screens.forEach {
+                    MainBottomNavItem(
+                        navHostController = navHostController,
+                        currentDestination = currentDestination,
+                        screen = it
+                    )
+                }
+
             }
 
         }
