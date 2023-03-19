6abb99d1c1c9a771d82efa2e22631f6985ea9de4
KenStarry <starrycodes@gmail.com>
2023-03-19 19:57:15 +0300

    Modified themes file

--- app/src/main/java/com/kenstarry/harrypotter/ui/theme/Color.kt
+++ app/src/main/java/com/kenstarry/harrypotter/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//  my colors
+val BackgroundLight = Color(0xfff8edeb)
+val BackgroundLightAlt = Color(0xffffffff)
+val BackgroundDark = Color(0xff2d2e2e)
+val BackgroundDarkAlt = Color(0xff0f0f0f)
+
+val accent = Color(0xffffb5a7)
+val accentLight = Color(0xfffcd5ce)
--- app/src/main/java/com/kenstarry/harrypotter/ui/theme/Theme.kt
+++ app/src/main/java/com/kenstarry/harrypotter/ui/theme/Theme.kt
@@ -3,28 +3,29 @@ package com.kenstarry.harrypotter.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = accent,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = accentLight,
+    onPrimary = BackgroundDark,
+    onSecondary = BackgroundDarkAlt
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = accent,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = accentLight,
+    onPrimary = BackgroundLight,
+    onSecondary = BackgroundLightAlt
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -52,11 +53,24 @@ fun HarryPotterTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val systemUiController = rememberSystemUiController()
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+
+            //  change status bar background and foreground icon colors
+            systemUiController.setStatusBarColor(
+                color = colorScheme.onPrimary,
+                darkIcons = !darkTheme
+            )
+
+            //  change navigation bar background and foreground icon colors
+            systemUiController.setNavigationBarColor(
+                color = colorScheme.onPrimary,
+                darkIcons = !darkTheme
+            )
         }
     }
 
