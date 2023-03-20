d36af77b7cf8ce905b9a97bea6c0827c4ed4d2e6
KenStarry <starrycodes@gmail.com>
2023-03-20 18:25:21 +0300

    Added OkHttpClient in Retrofit Module

---
+++ app/src/main/java/com/kenstarry/harrypotter/core/data/MyInterceptor.kt
@@ -0,0 +1,12 @@
+package com.kenstarry.harrypotter.core.data
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class MyInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+
+    }
+}
--- app/src/main/java/com/kenstarry/harrypotter/di/RetrofitModule.kt
+++ app/src/main/java/com/kenstarry/harrypotter/di/RetrofitModule.kt
@@ -1,6 +1,7 @@
 package com.kenstarry.harrypotter.di
 
 import com.kenstarry.harrypotter.core.data.HarryPotterApi
+import com.kenstarry.harrypotter.core.data.MyInterceptor
 import com.kenstarry.harrypotter.core.domain.use_case.GetCharacters
 import com.kenstarry.harrypotter.core.domain.use_case.HarryPotterUseCases
 import com.kenstarry.harrypotter.core.presentation.util.Constants.Companion.BASE_URL
@@ -8,8 +9,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -18,10 +21,23 @@ object RetrofitModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient
+        .Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(MyInterceptor())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient
+    ): Retrofit = Retrofit
         .Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 
     @Provides
